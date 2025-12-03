a4adcf0cb447e3ed297c1843aeb3b1d7b26da259
Mohammad <msepehrim@gmail.com>
2025-12-03 10:26:50 +0330

    bug fixed last line of input must have CRLF

--- AdventOfCode2025/day01-02.py
+++ AdventOfCode2025/day01-02.py
@@ -1,10 +1,10 @@
 import os
 pointer = 50
 password = 0
-if os.path.exists("out2.txt"):
-    os.remove("out2.txt")
-with open("out2.txt", "a") as out_file:
-    with open("day01_input_test.txt") as f:
+if os.path.exists("out-01-02.output"):
+    os.remove("out-01-02.output")
+with open("out-01-02.output", "a") as out_file:
+    with open("day01_input.txt") as f:
         for line in f:
             #out_file.write(line[0:-1] + " >>> " + line[1:])
             direction = line[0]
@@ -12,7 +12,7 @@ with open("out2.txt", "a") as out_file:
             rotation = int(line[1:-1])
             hit_with_rotation = int(rotation / 100)
             if direction == 'L':
-                if pointer - (rotation % 100) < 0:           
+                if pointer != 0 and pointer - (rotation % 100) < 0:           
                     password  = password + 1
 
                 pointer = pointer - rotation                
