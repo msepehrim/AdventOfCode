121d18396199412eeb45dedc0fe0cd3b59a1a5be
Mohammad <msepehrim@gmail.com>
2025-12-21 17:02:14 +0330

    day10 part one

--- AdventOfCode2025/day10-01.py
+++ AdventOfCode2025/day10-01.py
@@ -1,3 +1,21 @@
+class Diagram:
+    class Light:
+        def __init__(self, light):
+            self.light = light
+            self.convert()
+        def convert(self):
+            light = self.light.replace(".", "0").replace("#", "1")
+            self.light_number = int(light, 2)
+
+    class Button:
+        pass
+    class Joltage:
+        pass
+
+
 with open("day10_input_test.txt") as input_file:
     lines = input_file.readlines()
+diagram = Diagram()
+for line in lines:
+    light = Diagram.Light(line.strip(' ')[0])
     
